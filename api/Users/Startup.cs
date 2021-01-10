30c0135dc171d825e7b275e455d2741e34b8ec3e
Alex Cook <acook8@gmail.com>
2021-01-10 05:16:44 +0000

    Removed kestrel http redirect to https

--- api/Users/Startup.cs
+++ api/Users/Startup.cs
@@ -37,7 +37,7 @@ namespace Users
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
