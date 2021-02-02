4153ef9534af59d84585b83b9b714d4d4c998484
Alex Cook <acook8@gmail.com>
2021-02-02 23:18:03 +0000

    setup testing pipeline

--- testing/functionalTests/usersSelenium_test.py
+++ testing/functionalTests/usersSelenium_test.py
@@ -4,6 +4,10 @@ from seleniumbase import BaseCase
 class UsersTestClass(BaseCase):
     
     def test_usersPage(self):
+        # create test data
+        # create user with api
+        # verify user exists on ui
+        # delete user with api
         url = "https://dev.playground.alexcook.dev"
         self.open(url)
         self.assert_text('DevOps Playground')
